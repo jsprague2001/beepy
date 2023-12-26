6d7cdce544689cac284933306a5521c31335e556
Jim Sprague <jsprague@redarcgroup.com>
2023-12-26 16:28:35 -0500

    Create battstat.py

---
+++ code/battstat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import os
+
+batt_percent_file = '/sys/firmware/beepy/battery_percent'
+
+# Get battery percentage
+with open(batt_percent_file) as f:
+    batt_percent = f.readline()
+
+batt_percent_num = int(batt_percent)
+
+def switch(batt_percent_num):
+    if 31 <= batt_percent_num < 100:
+        return '-w'
+    if 21 <= batt_percent_num <= 30:
+        return '-s'
+    if 11 <= batt_percent_num <= 20:
+        return '-t'
+    if 0 <= batt_percent_num <= 10:
+        return '-d'
+
+cmd = 'cowsay ' + switch(batt_percent_num) + ' Battery ' + batt_percent + '%'
+# print(cmd)
+returned_value = os.system(cmd)
